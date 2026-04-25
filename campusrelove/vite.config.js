80180b7775d6af4212c7d8b99395ebce78b72dfd
Afandra <dafagiandradwimaulana@gmail.com>
2026-04-26 00:37:04 +0700

    Initial commit - CampusRelove

--- campusrelove/vite.config.js
+++ campusrelove/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/campusrelove/',
+  build: {
+    outDir: 'docs',  // GitHub Pages bisa deploy dari /docs
+  },
 })
